0b90f6384fb79494d8abab2e53398b5a7ac9589e
Shaun Luttin <shaunluttin@gmail.com>
2013-09-13 01:47:57 -0700

    Add a bunch of reasources that have helped me to learn.

---
+++ Vendord/HelloCOM/HelloCOM.cpp
@@ -0,0 +1,15 @@
+// HelloCOM.cpp : Defines the entry point for the console application.
+//
+
+#include "stdafx.h"
+
+using namespace std;
+
+int _tmain(int argc, _TCHAR* argv[])
+{
+
+	int i; 
+	cout<< "Hello World !" <<endl;
+	getchar();
+	return 0;
+}
---
+++ Vendord/VendordConsole/Program.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Runtime.InteropServices;
+using AS = OpenNETCF.Desktop.Communication;
+
+namespace VendordConsole
+{
+
+    class Program
+    {
+        [DllImport("User32.dll")]
+        static extern Boolean MessageBeep(UInt32 beepType);
+
+        [DllImport("Ceutil.dll")]
+        static extern int CeGetDeviceId();  
+
+        private static AS.RAPI rapi;
+
+        private static void On_RapiConnected()
+        {
+            Console.WriteLine("Connected");
+        }
+
+        // see also http://community.psion.com/knowledge/w/knowledgebase/1330.rapi-remote-api-to-talk-with-windows-ce-device-via-active-sync.aspx
+        static void Main(string[] args)
+        {
+            rapi = new AS.RAPI();
+
+            if (rapi.DevicePresent)
+            {
+                Console.WriteLine("DevicePresent");
+
+                if (!rapi.Connected)
+                {
+                    rapi.RAPIConnected += On_RapiConnected;
+                    rapi.Connect(false);
+                }
+
+                AS.SYSTEM_INFO info;
+                rapi.GetDeviceSystemInfo(out info);
+
+                Console.WriteLine(Enum.GetName(typeof(AS.ProcessorArchitecture), info.wProcessorArchitecture));
+                Console.WriteLine(Enum.GetName(typeof(AS.ProcessorType), info.dwProcessorType));
+
+                IEnumerable<AS.FileInformation> e = rapi.EnumerateFiles("*");
+                Console.WriteLine(e.Count());
+                foreach (AS.FileInformation fileinfo in e)
+                {
+                    Console.WriteLine(fileinfo.FileName);
+                }
+
+                if (rapi.DeviceFileExists("Vendord_Testing"))
+                {
+                    Console.WriteLine("Exists");
+                    rapi.RemoveDeviceDirectory("Vendord_Testing");
+                }                
+            }
+
+            //int i = CeGetDeviceId();
+            //MessageBeep(0);
+
+            Console.ReadLine();
+        }
+    }
+}
