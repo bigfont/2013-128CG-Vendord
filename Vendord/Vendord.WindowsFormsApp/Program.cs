9a553f60d9b781d10f9587f7c420afb1a61675ca
Shaun Luttin <shaunluttin@gmail.com>
2013-09-13 11:30:03 -0700

    Add a WindowsFormsApp and a SqlServerApp for code separation.

--- Vendord/Vendord.WindowsFormsApp/Program.cs
+++ Vendord/Vendord.WindowsFormsApp/Program.cs
@@ -1,19 +1,21 @@
 ﻿using System;
-using System.Linq;
 using System.Collections.Generic;
+using System.Linq;
 using System.Windows.Forms;
 
-namespace Vendord.SmartDeviceApp
+namespace Vendord.WindowsFormsApp
 {
     static class Program
     {
         /// <summary>
         /// The main entry point for the application.
         /// </summary>
-        [MTAThread]
+        [STAThread]
         static void Main()
-        {            
-            Application.Run(new MainForm());
+        {
+            Application.EnableVisualStyles();
+            Application.SetCompatibleTextRenderingDefault(false);
+            Application.Run(new Form1());
         }
     }
 }
