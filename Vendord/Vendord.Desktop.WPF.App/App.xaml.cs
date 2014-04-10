c2c3b252f4d260a34db3a493115891dbc96cfefe
Shaun Luttin <shaunluttin@gmail.com>
2014-04-10 09:12:30 -0700

    Start on WPF UI for Vendord Desktop.

---
+++ Vendord/Vendord.Desktop.WPF.App/App.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Data;
+using System.Linq;
+using System.Windows;
+using Vendord.Desktop.WPF.App.ViewModel;
+
+namespace Vendord.Desktop.WPF.App
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            base.OnStartup(e);
+
+            MainWindow window = new MainWindow();
+
+            // Create the ViewModel to which 
+            // the main window binds.
+            string path = "Data/customers.xml";
+            var viewModel = new MainWindowViewModel(path);
+
+            // When the ViewModel asks to be closed, 
+            // close the window.
+            EventHandler handler = null;
+            handler = delegate
+            {
+                viewModel.RequestClose -= handler;
+                window.Close();
+            };
+            viewModel.RequestClose += handler;
+
+            // Allow all controls in the window to 
+            // bind to the ViewModel by setting the 
+            // DataContext, which propagates down 
+            // the element tree.
+            window.DataContext = viewModel;
+
+            window.Show();
+        }
+
+    }
+}
