71179301243127b9c81647460f7e208f4cdf7c7a
Shaun Luttin <shaunluttin@gmail.com>
2014-04-15 09:49:36 -0700

    Add a Vendord.Sync proj.

---
+++ Vendord/References/_readme.txt
@@ -0,0 +1,8 @@
+# SqlServerCe
+- "C:\Program Files (x86)\Microsoft SQL Server Compact Edition\v3.5\Desktop\System.Data.SqlServerCe.dll"
+
+# Microsoft.Syncronization
+- "C:\Program Files (x86)\Microsoft SDKs\Microsoft Sync Framework\2.1\Runtime\x86\Microsoft.Synchronization.dll"
+- "C:\Program Files (x86)\Microsoft SDKs\Microsoft Sync Framework\2.1\Runtime\ADO.NET\V3.1\x86\Microsoft.Synchronization.Data.dll"
+- "C:\Program Files (x86)\Microsoft SDKs\Microsoft Sync Framework\2.1\Runtime\ADO.NET\V3.1\x86\Microsoft.Synchronization.Data.SqlServer.dll"
+- "C:\Program Files (x86)\Microsoft SDKs\Microsoft Sync Framework\2.1\Runtime\ADO.NET\V3.1\x86\Microsoft.Synchronization.Data.SqlServerCe.dll"
--- Vendord/Vendord.Sync/Properties/AssemblyInfo.cs
+++ Vendord/Vendord.Sync/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("DatabaseTests")]
+[assembly: AssemblyTitle("Vendord.Sync")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("DatabaseTests")]
+[assembly: AssemblyProduct("Vendord.Sync")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("e5205619-ab95-49de-bba7-f665c4dcb2e6")]
+[assembly: Guid("7f24afaf-6727-40de-a1b0-ae8fc7396c33")]
 
 // Version information for an assembly consists of the following four values:
 //
